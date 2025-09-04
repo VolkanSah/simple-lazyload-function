992ced6f1eb6ce5c396178017bb67f2c9ba53019
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-09-04 16:31:01 +0200

    Update lazyload.js
    smaller code wirth observe

--- lazyload.js
+++ lazyload.js
@@ -1,22 +1,14 @@
-<script>
-        document.addEventListener("DOMContentLoaded", function() {
-            let lazyImages = [].slice.call(document.querySelectorAll("img.lazyload"));
-            if ("IntersectionObserver" in window) {
-                let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-                    entries.forEach(function(entry) {
-                        if (entry.isIntersecting) {
-                            let lazyImage = entry.target;
-                            lazyImage.src = lazyImage.dataset.src;
-                            lazyImage.classList.remove("lazyload");
-                            lazyImageObserver.unobserve(lazyImage);
-                        }
-                    });
-                });
-                lazyImages.forEach(function(lazyImage) {
-                    lazyImageObserver.observe(lazyImage);
-                });
-            } else {
-                // Fallback für Browser, die IntersectionObserver nicht unterstützen
-            }
-        });
+<script defer>
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            img.classList.remove("lazyload");
+            observer.unobserve(img);
+        }
+    });
+});
+
+document.querySelectorAll("img.lazyload").forEach(img => observer.observe(img));
 </script>
